17ef44a554f7328085db01bf9fc7ca085f92116d
Ivan <i.vasilev@dunice.net>
2022-11-10 15:45:39 +0300

    Add project files.

---
+++ OpenGL_3D/Application.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <GLAD/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <GLM/glm.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+#include <GLM/gtc/type_ptr.hpp>
+
+#include "stb_image.h"
+#include "shader_s.h"
+#include "camera.h"
+
+#include <iostream>
+
+class Application
+{};
